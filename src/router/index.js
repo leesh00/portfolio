d0c87b56e85da563befeee7541017d44265814a9
sohiLee <lsohi2468@gmail.com>
2025-08-03 19:16:34 +0900

    about 삭제

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import Projects from '../pages/Projects.vue'
-import About from '../pages/About.vue'
 import Contact from '../pages/Contact.vue'
 
 const routes = [
