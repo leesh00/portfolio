2062ed1176fad82c450b5f9a47eaafd1aa6c2b1b
sohiLee <lsohi2468@gmail.com>
2025-09-27 03:58:02 +0900

    프로젝트 추가

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -3,18 +3,14 @@
     <h1>주요 프로젝트</h1>
     <!-- project 1 -->
     <div class="project__item">
-      <h3 class="project__name">
-        <p>CJ 온스타일</p>
-      </h3>
+      <h3 class="project__name">CJ 온스타일</h3>
       <ul class="project__meta">
-        <li>- 2025.05 ~ ing</li>
-        <li>- Vue.js / Nuxt.js / Typescript</li>
-        <!-- <li>기존 모듈에 기능을 안정적으로 확장하며 리팩토링을 통해 중복 로직 제거 및 코드 품질 개선을 수행</li> -->
-        <!-- <li>기간이 정해진 이벤트 신청 기능 개발 시, 회원 정보와 참가여부 API를 연동하여 사용자 등급에 따라 UI 및 신청 조건을 동적으로 제어</li> -->
+        <li>- 25.05 ~ ing</li>
+        <li>- Vㅉue.js / Nuxt.js / Typescript</li>
       </ul>
       <div class="project__details">
         <ul>
-          <li class="sub_con">
+          <li class="sub_con img_box">
             <div class="sub_img">
               <img src="../images/project-01.jpg" alt="">
             </div>
@@ -49,7 +45,6 @@
                   </p>
                   <div>
                     <img src="../images/project-02.jpg" alt="">
-
                   </div>
                 </li>                 
               </ul>
@@ -58,50 +53,65 @@
         </ul>
       </div>
     </div>
-
     <!-- project 2 -->
-    <!-- <div class="project__item">
-      <div class="project__thumb">
-        <img src="../images/project-01.jpg" alt="CJ ENM의 '셀렙샵' 유지보수">
-      </div>
-      <h3 class="project__name">CJ ENM의 '셀렙샵' 구축 및 유지보수</h3>
+    <div class="project__item">
+      <h3 class="project__name celebshop">셀렙샵 구축 및 운영</h3>
       <ul class="project__meta">
-        <li>기간: 2023.01 ~ 2024.08 : 구축</li>
-        <li>기간: 2024.08 ~ 2025.05 : 운영 유지보수</li>
-        <li>기술: Vue.js, Vuex, SCSS</li>
+        <li>- 10만회 이상 다운로드된 CJ ENM의 패션 전문 플랫폼 구축 및 운영</li>
+        <li>- 구축 23.01 ~ 24.08   /  운영 24.09 ~ 25.04</li> 
+        <!-- <li>기존 모듈에 기능을 안정적으로 확장하며 리팩토링을 통해 중복 로직 제거 및 코드 품질 개선을 수행</li> -->
+        <!-- <li>기간이 정해진 이벤트 신청 기능 개발 시, 회원 정보와 참가여부 API를 연동하여 사용자 등급에 따라 UI 및 신청 조건을 동적으로 제어</li> -->
       </ul>
       <div class="project__details">
         <ul>
-          <li>
-            <strong>인클루드 배너</strong>
-            <ul>
-              <li>스크롤 위치에 따라 진입 시 페이지 최상단에 배너 고정</li>
-              <li>상단 제목과 안내영역은 유지되며 배너만 아래 고정 위치로 전환</li>
-              <li>스크롤 업 시 제목은 숨겨지고 배너는 계속 고정된 상태로 유지</li>
-              <li>안내영역 닫힘을 감지해 배너 고정 위치를 자동 재조정</li>
-              <li>배너 클릭 시 해당 모듈로 부드럽게 이동</li>
-            </ul>
+          <li class="sub_con">
+            <div class="sub_txt">
+            <div class="tit">운영 및 유지보수</div>
+              <ul>
+                <li>
+                ▶ 1년간의 운영 및 유지보수
+                  <p>
+                      - 빠르게 변화하는 패션 사업에 대응해 이벤트·기획전 이슈를 <em>실시간으로 처리</em>
+                  </p>
+                  <p>
+                      - <em>직접 이벤트에 응모</em>해 이벤트 초반 <em>오류를 발견</em>하여 피해 발생 전 문제 해결셀렙샵 구축 및 운영
+
+                  </p>
+                </li>
+                <li>
+                ▶ 셀렙샵 구축
+                  <p>
+                      - 구축시 여러 모듈을 조합하여 구성하는 기획전 페이지 전담
+                  </p>
+                  <p>
+                      - 기획자, 디자이너, 운영자와 <em>지속적으로 소통</em>하며 실제 사용 환경을 고려하여 개발
+                  </p>
+                </li>                
+              </ul>
+            </div>
           </li>
-          <li>
-            <strong>댓글 기능 (기여도: 40%)</strong>
-            <ul>
-              <li>로그인 상태에 따라 프로필 닉네임이 자동 적용된 작성 폼 토글</li>
-              <li>답글 버튼 클릭 시 대댓글 입력폼을 노출하고 상위 댓글 ID와 함께 전송해 트리 구조로 렌더링</li>
-              <li>본인 댓글에만 수정·삭제 아이콘을 표시하고, 확인 모달 후 즉시 제거</li>
-              <li>댓글 깊이에 따라 들여쓰기 및 구분선 적용</li>
-            </ul>
-          </li>
-          <li>
-            <strong>유지보수</strong>
-             <ul>
-              <li>빠르게 변화하는 패션사업에 대응하여 단기 이벤트/기획전 페이지 생성 및 유지보수</li>
-              <li>로딩 속도 개선을 위해 API 요청 및 컴포넌트 렌더링을 비동기 처리하여 사용자 체감 속도 향상</li>
-              <li>이벤트 신청을 위한 회원정보</li>
-            </ul>
+          <li class="sub_con img_box">
+            <div class="sub_img">
+              <img class="sub_img_content" src="../images/project-celebshop.gif" alt="">
+              <img class="sub_img_frame" src="../images/iphone_frame.png" alt="">
+            </div>
+            <div class="sub_txt">
+            <div class="tit">인클루드 배너</div>
+              <ul>
+                <li>
+                  ▶ 모듈간의 호환성 중시
+                  <p>스크롤 위치에 따라 진입 시 페이지 최상단에 배너 고정</p>
+                  <p>상단 제목과 안내영역은 유지되며 배너만 아래 고정 위치로 전환</p>
+                  <p>스크롤 업 시 제목은 숨겨지고 배너는 계속 고정된 상태로 유지</p>
+                  <p>안내영역 닫힘을 감지해 배너 고정 위치를 자동 재조정</p>
+                  <p>배너 클릭 시 해당 모듈로 부드럽게 이동</p>
+                </li>               
+              </ul>
+            </div>
           </li>
         </ul>
       </div>
-    </div> -->
+    </div>
     <div class="project__item">
       <h3 class="project__retrospect-title">회고</h3>
       <p class="project__retrospect">
