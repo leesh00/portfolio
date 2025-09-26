79bbc0361534b4ff507a6535232ccb189af9c5d3
sohiLee <lsohi2468@gmail.com>
2025-09-26 23:12:40 +0900

    build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
-  base: '/portfolio/',
-  plugins: [vue()]
+  base: '/portfolio/', // 그대로 유지해도 좋아요
+  plugins: [vue()],
+  build: {
+    outDir: path.resolve(__dirname, 'docs') // 빌드 결과물을 docs 폴더에 저장
+  }
 })
